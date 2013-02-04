51fe2d848535e9e26af786a1b74ca01ab6a1246f
Markus Hausammann <markus.hausammann@cloud-solutions.net>
2013-02-04 23:07:17 +0200

    initial commit of module and sentry writer, functional

---
+++ src/ZendSentry/Log/Writer/Sentry.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Sentry Writer for the Zend Framework Logger
+ */
+namespace ZendSentry\Log\Writer;
+
+use Zend\Log\Writer\AbstractWriter;
+use Zend\Log\Logger;
+use Raven_Client as Raven;
+
+class Sentry extends AbstractWriter
+{
+    /**
+     * Translates Zend Framework log levels to Raven log levels.
+     */
+    private $logLevels = array(
+        'DEBUG'     => Raven::DEBUG,
+        'INFO'      => Raven::INFO,
+        'NOTICE'    => Raven::INFO,
+        'WARN'      => Raven::WARNING,
+        'ERR'       => Raven::ERROR,
+        'CRIT'      => Raven::FATAL,
+        'ALERT'     => Raven::FATAL,
+        'EMERG'     => Raven::FATAL,
+    );
+
+    protected $raven;
+
+    public function __construct($options, Raven $raven)
+    {
+        $this->raven = $raven;
+        parent::__construct($options);
+    }
+
+    /**
+     * Write a message to the log
+     *
+     * @param array $event log data event
+     * @return void
+     */
+    protected function doWrite(array $event)
+    {
+        $extra['timestamp'] = $event['timestamp'];
+        $this->raven->captureMessage($event['message'], array(), $this->logLevels[$event['priorityName']], false, $event['extra']);
+    }
+}

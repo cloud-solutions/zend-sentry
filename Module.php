38f8d457881161f6f05ed562b70d23119b40eb95
Markus Hausammann <markus.hausammann@cloud-solutions.net>
2013-02-10 05:36:30 +0200

    replace default zf ExceptionStrategy

--- Module.php
+++ Module.php
@@ -2,8 +2,10 @@
 
 namespace ZendSentry;
 
-use Zend\EventManager\StaticEventManager;
+use Zend\EventManager\EventManager;
 use Zend\Mvc\MvcEvent;
+use ZendSentry\Mvc\View\Http\ExceptionStrategy as SentryStrategy;
+use Zend\Mvc\View\Http\ExceptionStrategy;
 use Raven_Client as Raven;
 use Zend\Log\Logger;
 use ZendSentry\Log\Writer\Sentry;
@@ -11,37 +13,58 @@ use ZendSentry\ZendSentry;
 
 class Module
 {
+    /**
+     * @var Raven $ravenClient
+     */
+    private $ravenClient;
+
+    /**
+     * @var ZendSentry $zendSentry
+     */
+    private $zendSentry;
+
+    /**
+     * @var $config
+     */
+    private $config;
+
+    /**
+     * @var EventManager $eventManager
+     */
+    private $eventManager;
+
+    /**
+     * @param MvcEvent $event
+     */
     public function onBootstrap(MvcEvent $event)
     {
-        // Setup the Zend Logger with our Sentry Writer
-        $config = $event->getApplication()->getServiceManager()->get('Config');
-        $sentryApiKey = $config['zend-sentry']['sentry_api_key'];
+        // Setup RavenClient (provided by Sentry) and Sentry (provided by this module)
+        $this->config = $event->getApplication()->getServiceManager()->get('Config');
+        $sentryApiKey = $this->config['zend-sentry']['sentry_api_key'];
+        $this->ravenClient = new Raven($sentryApiKey);
+        $this->zendSentry = new ZendSentry($this->ravenClient);
 
-        $logger      = new Logger;
-        $ravenClient = new Raven($sentryApiKey);
-        $writer      = new Sentry($ravenClient);
-        $logger->addWriter($writer);
+        // Get the eventManager and set it as a member for convenience
+        $this->eventManager = $event->getApplication()->getEventManager();
 
-        // Attach a logging listener for the log event on application level
-        $events = StaticEventManager::getInstance();
-        $events->attach('*', 'log', function($event) use ($logger) {
-            $target   = get_class($event->getTarget());
-            $message  = $event->getParam('message', 'No message provided');
-            $priority = (int) $event->getParam('priority', Logger::INFO);
-            $message  = sprintf('%s: %s', $target, $message);
-            $logger->log($priority, $message);
-        });
+        // Setup the possibility to send custom logs to Sentry
+        $this->setupBasicLogging($event);
 
-        // Use the ZendSentry convenience methods to register different handlers if enabled in the module config
-        $zendSentry = new ZendSentry($ravenClient);
-
-        if ($config['zend-sentry']['handle-errors']) {
-            $zendSentry->registerErrorHandler();
+        // If ZendSentry is configured to log exceptions, a few things need to be set up
+        if ($this->config['zend-sentry']['handle-exceptions']) {
+            $this->setupExceptionLogging($event);
         }
 
-        if ($config['zend-sentry']['handle-exceptions']) {
-            $zendSentry->registerExceptionHandler();
+        // If ZendSentry is configured to log errors, register it as error handler
+        if ($this->config['zend-sentry']['handle-errors']) {
+            $this->zendSentry->registerErrorHandler();
         }
+
+        // If ZendSentry is configured to log shutdown errors, register it
+        if ($this->config['zend-sentry']['handle-shutdown-errors']) {
+            $this->zendSentry->registerShutdownFunction();
+        }
+
     }
 
     public function getAutoloaderConfig()
@@ -54,5 +77,58 @@ class Module
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
+    }
+
+    /**
+     * Gives us the possibility to write logs to Sentry from anywhere in the application
+     *
+     * @param MvcEvent $event
+     */
+    private function setupBasicLogging(MvcEvent $event)
+    {
+         // Setup the Zend Logger with our Sentry Writer
+        $logger      = new Logger;
+        $writer      = new Sentry($this->ravenClient);
+        $logger->addWriter($writer);
+
+        // Get the shared event manager and attach a logging listener for the log event on application level
+        $sharedManager = $this->eventManager->getSharedManager();
+
+        $sharedManager->attach('*', 'log', function($event) use ($logger) {
+            /** @var $event MvcEvent */
+            $target   = get_class($event->getTarget());
+            $message  = $event->getParam('message', 'No message provided');
+            $priority = (int) $event->getParam('priority', Logger::INFO);
+            $message  = sprintf('%s: %s', $target, $message);
+            $logger->log($priority, $message);
+        }, 2);
+    }
+
+    /**
+     * 1. Registers Sentry as exception handler
+     * 2. Replaces the default ExceptionStrategy so Exception that are caught by Zend Framework can still be logged
+     *
+     * @param MvcEvent $event
+     */
+    private function setupExceptionLogging(MvcEvent $event)
+    {
+        // Register Sentry as exception handler for exception that bubble up to the top
+        $this->zendSentry->registerExceptionHandler();
+
+        // Replace the default ExceptionStrategy with ZendSentry's strategy
+        /** @var $exceptionStrategy ExceptionStrategy */
+        $exceptionStrategy = $event->getApplication()->getServiceManager()->get('ViewManager')->getExceptionStrategy();
+        $exceptionStrategy->detach($this->eventManager);
+        $exceptionStrategy = new SentryStrategy;
+        $exceptionStrategy->attach($this->eventManager);
+        $exceptionStrategy->setDisplayExceptions($this->config['zend-sentry']['display-exceptions']);
+
+        $ravenClient = $this->ravenClient;
+        // Attach an exception listener for the ZendSentry exception strategy, can be triggered from anywhere else too
+        $this->eventManager->getSharedManager()->attach('*', 'logException', function($event) use ($ravenClient) {
+            /** @var $event MvcEvent */
+            $exception = $event->getParam('exception');
+            $ravenClient->captureException($exception);
+        });
     }
 }

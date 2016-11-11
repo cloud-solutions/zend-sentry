6b26e41333404e224d7024c78461cd4127e40608
Markus <markus.hausammann@cloud-solutions.net>
2016-11-11 12:29:05 +0200

    Revert "Add support for Zend Framework 3"

--- src/ZendSentry/Mvc/View/Http/ExceptionStrategy.php
+++ src/ZendSentry/Mvc/View/Http/ExceptionStrategy.php
@@ -7,13 +7,13 @@
  *
  * @package    ZendSentry\Mvc\View\Http\ExceptionStrategy
  * @license    New BSD License {@link /docs/LICENSE}
- * @copyright  Copyright (c) 2016, cloud solutions OÜ
+ * @copyright  Copyright (c) 2011, cloud solutions OÜ
  */
 
 namespace ZendSentry\Mvc\View\Http;
 
-use Zend\EventManager\AbstractListenerAggregate;
 use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\ListenerAggregateInterface;
 use Zend\Http\Response as HttpResponse;
 use Zend\Mvc\Application;
 use Zend\Mvc\MvcEvent;
@@ -26,7 +26,7 @@ use Zend\View\Model\ViewModel;
  *
  * @package    ZendSentry\Mvc\View\Http\ExceptionStrategy
  */
-class ExceptionStrategy extends AbstractListenerAggregate
+class ExceptionStrategy implements ListenerAggregateInterface
 {
     /**
      * Display exceptions?
@@ -44,15 +44,38 @@ class ExceptionStrategy extends AbstractListenerAggregate
      * Name of exception template
      * @var string
      */
-    protected $exceptionTemplate = 'error';
+    protected $exceptionTemplate = 'error/index';
 
     /**
-     * {@inheritDoc}
+     * @var \Zend\Stdlib\CallbackHandler[]
      */
-    public function attach(EventManagerInterface $events, $priority = 1)
+    protected $listeners = array();
+
+    /**
+     * Attach the aggregate to the specified event manager
+     *
+     * @param  EventManagerInterface $events
+     * @return void
+     */
+    public function attach(EventManagerInterface $events)
     {
         $this->listeners[] = $events->attach(MvcEvent::EVENT_DISPATCH_ERROR, array($this, 'prepareExceptionViewModel'));
         $this->listeners[] = $events->attach(MvcEvent::EVENT_RENDER_ERROR, array($this, 'prepareExceptionViewModel'));
+    }
+
+    /**
+     * Detach aggregate listeners from the specified event manager
+     *
+     * @param  EventManagerInterface $events
+     * @return void
+     */
+    public function detach(EventManagerInterface $events)
+    {
+        foreach ($this->listeners as $index => $listener) {
+            if ($events->detach($listener)) {
+                unset($this->listeners[$index]);
+            }
+        }
     }
 
     /**
@@ -80,7 +103,6 @@ class ExceptionStrategy extends AbstractListenerAggregate
     /**
      * Set the default exception message
      * @param string $defaultExceptionMessage
-     * @return self
      */
     public function setDefaultExceptionMessage($defaultExceptionMessage)
     {
@@ -141,7 +163,7 @@ class ExceptionStrategy extends AbstractListenerAggregate
             case Application::ERROR_EXCEPTION:
             default:
                 // check if there really is an exception
-                // ZF also throws normal errors, for example: error-route-unauthorized
+                // zf2 also throw normal errors, for example: error-route-unauthorized
                 // if there is no exception we have nothing to log
                 if ($e->getParam('exception') == null) {
                     return;

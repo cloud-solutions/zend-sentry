0b5b3dc3164500869b2d5af04078869f32636c86
Milan Divkovic <milan.divkovic@gmx.de>
2019-07-10 10:26:51 +0200

    Add option to specify location for raven-js to load from

--- Module.php
+++ Module.php
@@ -241,6 +241,12 @@ class Module
             $ravenjsVersion = $this->config['zend-sentry']['ravenjs-version'] ?? self::RAVENJS_VERSION;
             $cdnUri = sprintf('//cdn.ravenjs.com/%s/raven.min.js', $ravenjsVersion);
             $headScript->offsetSetFile(0, $cdnUri);
+        } else {
+            $ravenjsSource = $this->config['zend-sentry']['use-ravenjs-cdn'] ?? false;
+
+            if ($ravenjsSource) {
+                $headScript->offsetSetFile(0, $ravenjsSource);
+            }
         }
 
         $publicApiKey  = $this->convertKeyToPublic($this->config['zend-sentry']['sentry-api-key']);
